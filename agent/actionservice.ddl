2b8b9f3f134c4691a006d56262799a007b8cbc36
Richard Pearce <richard.pearce@timgroup.com>
2014-01-02 15:50:52 +0000

    Initial commit

---
+++ agent/actionservice.ddl
@@ -0,0 +1,26 @@
+metadata    :name        => "actionservice",
+            :description => "Start and stop system services",
+            :author      => "Richard Pearce (heavily borrowed from service.rb by R.I.Pienaar)",
+            :license     => "ASL2",
+            :version     => "1",
+            :timeout     => 60
+
+config = YAML.load_file('/etc/mcollective/action_services.yaml')
+config['services'].each do |service|
+  ['start','stop','restart','status'].each do |action|
+    action "#{action}-#{service}", :description => "#{action} #{service}" do
+      display :always
+
+      output :status,
+            :description => "The status of the service",
+            :display_as  => "Service Status",
+            :default     => "unknown"
+
+      summarize do
+            aggregate summary(:status)
+      end
+
+    end
+  end
+end
+
---
+++ agent/actionservice.rb
@@ -0,0 +1,34 @@
+require 'puppet'
+require 'yaml'
+
+module MCollective
+  module Agent
+    # An agent that receives an action and service.
+    # Allows acl's to be explicitly set per action/service.
+    # mco rpc actionservice status-elasticsearch-default
+    class Actionservice<RPC::Agent
+      config = YAML.load_file('/etc/action_services.yaml')
+      config['services'].each do |service|
+        ['start','stop','status','restart'].each do |action|
+          action_with_service = "#{action}-#{service}"
+          action "#{action}-#{service}" do
+            result =  Service.do_service_action(action, service)
+            if ['start','stop'].include? action 
+              if result[:msg]
+                reply[:status] = result[:status]
+                reply.fail! result[:msg]
+              else
+                reply[:status] = result[:status]
+              end
+            else
+              reply[:status] = result
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+
+# vi:tabstop=2:expandtab:ai:filetype=ruby
